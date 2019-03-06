a7dd12939cf35c2e7514b59f039d61c96bf932fd
Жанара <@DESKTOP-O9KC10I>
2019-03-07 01:38:19 +0600

    ++

--- TechnicalService/Console/Menu.cs
+++ TechnicalService/Console/Menu.cs
@@ -9,6 +9,7 @@ namespace TechnicalService.Console
 {
     public class Menu
     {
+       
         public void WelcomeMenu()
         {
             System.Console.WriteLine("Добро пожаловать!");
@@ -20,50 +21,74 @@ namespace TechnicalService.Console
         }
         public void MainMenu()
         {
-           
+
             System.Console.WriteLine("1. Управление\n2. Создание\n3. Выход");
             System.Console.WriteLine("\n Ваш выбор: ");
+
             int ch = int.Parse(System.Console.ReadLine());
-            if (ch == 1)
+            while (!(ch >= 4))
             {
-                ManageMenu();
-            }
-            else if (ch == 2)
-            {
-                CreationMenu();
-            }
-            else if (ch == 3)
-            {
-                System.Console.WriteLine("До свидания!");
-                Thread.Sleep(1500);
-                System.Environment.Exit(1);
+                if (ch == 1)
+                {
+                    System.Console.Clear();
+                    ManageMenu();
+                }
+                else if (ch == 2)
+                {
+                    System.Console.Clear();
+                    CreationMenu();
+                }
+                else if (ch == 3)
+                {
+                    System.Console.WriteLine("До свидания!");
+                    Thread.Sleep(1500);
+                    System.Environment.Exit(1);
+                }
+
+
+                if (ch >= 4)
+                {
+                    System.Console.WriteLine("Неверный выбор");
+                    System.Console.Clear();
+                    MainMenu();
+                }
+
+
             }
         }
-
         public void CreationMenu()
         {
             System.Console.WriteLine("1. Добавить машину\n2. Создать компонент машины\n3. Создать проект\n4. Создать пользователя\n5. Создать останову\n6. Назад\n");
             System.Console.WriteLine("\n Ваш выбор: ");
             int choice = int.Parse(System.Console.ReadLine());
-            switch (choice)
+            while (!(choice >= 7))
             {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    System.Console.Clear();
-                    MainMenu();
-                    
-                    break;
-                default:
-                    break;
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        System.Console.Clear();
+                        MainMenu();
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (choice >= 7)
+            {
+                System.Console.WriteLine("Неверный выбор");
+                System.Console.Clear();
+                CreationMenu();
             }
         }
 
@@ -72,25 +97,40 @@ namespace TechnicalService.Console
             System.Console.WriteLine("1. Машины\n2. Компоненты\n3. Проекты\n4. Пользователи\n5. Останова\n6. Назад\n");
             System.Console.WriteLine("\n Ваш выбор: ");
             int choice = int.Parse(System.Console.ReadLine());
-            switch (choice)
+            while (!(choice >= 7))
             {
-                case 1: CarMenu();
-                    break;
-                case 2: ComponentMenu();
-                    break;
-                case 3: ProjectMenu();
-                    break;
-                case 4:UserMenu();
-                    break;
-                case 5:CarBrokeMenu();
-                    break;
-                case 6:
-                    System.Console.Clear();
+                switch (choice)
+                {
+                    case 1:
+                        CarMenu();
+                        break;
+                    case 2:
+                        ComponentMenu();
+                        break;
+                    case 3:
+                        ProjectMenu();
+                        break;
+                    case 4:
+                        UserMenu();
+                        break;
+                    case 5:
+                        CarBrokeMenu();
+                        break;
+                    case 6:
+                        System.Console.Clear();
 
-                    MainMenu();
-                    break;
-                default:
-                    break;
+                        MainMenu();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (choice >= 7)
+            {
+                System.Console.WriteLine("Неверный выбор");
+                System.Console.Clear();
+                ManageMenu();
             }
         }
 
@@ -99,21 +139,30 @@ namespace TechnicalService.Console
             System.Console.WriteLine("1. Отображение всего парка машина по проектам\n2. Поиск машины по его гаражному номеру, модели\n3. Установить статус Активна/Неактивная\n4. Назад");
             System.Console.WriteLine("\n Ваш выбор: ");
             int choice = int.Parse(System.Console.ReadLine());
-            switch (choice)
+            while (!(choice >= 4))
             {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    System.Console.Clear();
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        System.Console.Clear();
 
-                    ManageMenu();
-                    break;
-                default:
-                    break;
+                        ManageMenu();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (choice >= 4)
+            {
+                System.Console.WriteLine("Неверный выбор");
+                System.Console.Clear();
+                CarMenu();
             }
         }
 
@@ -122,19 +171,29 @@ namespace TechnicalService.Console
             System.Console.WriteLine("1. Отобразить все компоненты на проекте\n2. Назад");
             System.Console.WriteLine("\n Ваш выбор: ");
             int choice = int.Parse(System.Console.ReadLine());
-            switch (choice)
+            while (!(choice >= 3))
             {
-                case 1:
-                    break;
-                case 2:
-                    System.Console.Clear();
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        System.Console.Clear();
 
-                    ManageMenu();
-                    break;
-                               
-                default:
-                    break;
+                        ManageMenu();
+                        break;
+
+                    default:
+                        break;
+                }
             }
+            if (choice >= 3)
+            {
+                System.Console.WriteLine("Неверный выбор");
+                System.Console.Clear();
+                ComponentMenu();
+            }
+
         }
 
         public void ProjectMenu()
@@ -142,17 +201,26 @@ namespace TechnicalService.Console
             System.Console.WriteLine("1. Отобразить весь список проектов\n2. Назад");
             System.Console.WriteLine("\n Ваш выбор: ");
             int choice = int.Parse(System.Console.ReadLine());
-            switch (choice)
+            while (!(choice >= 3))
             {
-                case 1:
-                    break;
-                case 2:
-                    System.Console.Clear();
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        System.Console.Clear();
 
-                    ManageMenu();
-                    break;
-                default:
-                    break;
+                        ManageMenu();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (choice >= 3)
+            {
+                System.Console.WriteLine("Неверный выбор");
+                System.Console.Clear();
+                ProjectMenu();
             }
         }
 
@@ -161,18 +229,28 @@ namespace TechnicalService.Console
             System.Console.WriteLine("1. Отобразить весь список пользователей по проектам\n2. Назад");
             System.Console.WriteLine("\n Ваш выбор: ");
             int choice = int.Parse(System.Console.ReadLine());
-            switch (choice)
+            while (!(choice >= 3))
             {
-                case 1:
-                    break;
-                case 2:
-                    System.Console.Clear();
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        System.Console.Clear();
 
-                    ManageMenu();
-                    break;
-                default:
-                    break;
+                        ManageMenu();
+                        break;
+                    default:
+                        break;
+                }
             }
+            if (choice >= 3)
+            {
+                System.Console.WriteLine("Неверный выбор");
+                System.Console.Clear();
+                UserMenu();
+            }
+
         }
 
         public void CarBrokeMenu()
@@ -180,22 +258,28 @@ namespace TechnicalService.Console
             System.Console.WriteLine("1. Отобразить весь список остановов\n2. Назад");
             System.Console.WriteLine("\n Ваш выбор: ");
             int choice = int.Parse(System.Console.ReadLine());
-            switch (choice)
+            while (!(choice >= 3))
             {
-                case 1:
-                    break;
-                case 2:
-                    System.Console.Clear();
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        System.Console.Clear();
 
-                    ManageMenu();
-                    break;
-                default:
-                    break;
+                        ManageMenu();
+                        break;
+                    default:
+                        break;
+                }
             }
+            if (choice >= 3)
+            {
+                System.Console.WriteLine("Неверный выбор");
+                System.Console.Clear();
+                CarBrokeMenu();
+            }
+
         }
-
-
-
-
     }
 }
